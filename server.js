699d9e8f285d1050a9e5d968cdf51623b6fe9dde
Kendall Buchanan <kendall@kendagriff.com>
2016-04-23 15:25:20 -0600

    complete exercise

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var mongoose = require('mongoose');
 
+var port = 3000;
+
 var app = express();
 app.use(bodyParser.json());
 mongoose.set("debug", true); // log connection methods
@@ -11,26 +13,39 @@ mongoose.connection.once("open", function() {
   console.log("Connected to MongoDB");
 });
 
-var port = 3000;
+var Sighting = require("./Sighting");
 
 app.post('/api/sighting', function(req, res) {
-  console.log('POST sighting');
-  res.end();
+  var sighting = new Sighting(req.body);
+  sighting.save(function(err, s) {
+    return err ? res.status(500).send(err) : res.send(s);
+  });
 });
  
 app.get('/api/sighting', function(req, res) {
-  console.log('GET sighting');
-  res.end();
+  Sighting.find({status: req.query.status}, function(err, sightings) {
+    return res.send(sightings);
+  });
 });
  
 app.delete('/api/sighting', function(req, res) {
-  console.log('DELETE sighting');
-  res.end();
+  Sighting.findByIdAndRemove(req.query.id, function(err, sighting) {
+    return res.send("Success");
+  });
 });
  
 app.put('/api/sighting', function(req, res) {
-  console.log('PUT sighting');
-  res.end();
+  Sighting.findById(req.query.id, function(err, sighting) {
+    sighting.update(req.body, function(err, s) {
+      if (err) {
+        return res.status(500).send(err);
+      } else {
+        Sighting.findById(req.query.id, function(e, s) {
+          return res.send(s);
+        });
+      }
+    });
+  });
 });
 
 app.listen(port, function() {
