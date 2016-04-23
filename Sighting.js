df4ea8538d35e95892dcb152fc74fb23f5abeb29
Kendall Buchanan <kendall@kendagriff.com>
2016-04-23 15:15:57 -0600

    add Sighting schema and model

---
+++ Sighting.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var sightingSchema = new Schema({
+  name: {type: String, lowercase: true},
+  order: {type: String, lowercase: true, maxlength: 20},
+  status: {
+    type: String,
+    lowercase: true,
+    enum: [
+      "extinct",
+      "near threatened",
+      "least concern"
+    ]
+  },
+  confirmed: {type: Boolean, default: false},
+  numberSeen: {type: Number, min: 1}
+});
+
+module.exports = mongoose.model("Sighting", sightingSchema);
